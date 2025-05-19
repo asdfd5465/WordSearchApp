c43b4a0edf0a6e37b141c160d9b83cd2cec8ba9d
asdfd5465 <forfirestick25@gmail.com>
2025-05-19 12:30:27 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,25 @@
 // WordFinderApp/build.gradle.kts
+// This file can be intentionally blank if plugins are managed in settings.gradle.kts
+// and applied in module-level build.gradle.kts.
+// However, often you define top-level plugin versions here using the older `buildscript`
+// if not using version catalogs.
+//
+// Given your setup, and if you create libs.versions.toml as hinted above, this works:
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
 
-// If you don't have libs.versions.toml, you can define versions directly or
-// use a more traditional approach for now. For CLI, simpler might be:
-// plugins {
-//    id("com.android.application") version "8.3.0" apply false
-//    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-// }
-//
-// To make the alias above work without libs.versions.toml, create a file:
-// WordFinderApp/gradle/libs.versions.toml
-// and add:
-// [versions]
-// agp = "8.3.0"
-// kotlin = "1.9.22"
-//
-// [plugins]
-// android-application = { id = "com.android.application", version.ref = "agp" }
-// jetbrains-kotlin-android = { id = "org.jetbrains.kotlin.android", version.ref = "kotlin" }
-
-// For simplicity without libs.versions.toml yet, let's use this in project build.gradle.kts:
+// If NOT using libs.versions.toml, for this CLI setup, you can do:
 // buildscript {
+//     ext.kotlin_version = "1.9.22" // Example
 //     repositories {
 //         google()
 //         mavenCentral()
 //     }
 //     dependencies {
-//         classpath("com.android.tools.build:gradle:8.3.0")
-//         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+//         classpath 'com.android.tools.build:gradle:8.3.0' // AGP
+//         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
 //     }
 // }
 //
@@ -40,7 +29,3 @@ plugins {
 //         mavenCentral()
 //     }
 // }
-//
-// The pluginManagement in settings.gradle.kts is the modern way,
-// so let's stick to that and assume the aliases will resolve if you set up libs.versions.toml
-// or use explicit plugin IDs in app/build.gradle.kts
