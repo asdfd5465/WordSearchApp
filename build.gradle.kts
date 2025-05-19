5b8b9afbb6e25c3ead9d8e22ac6f976e09e179c9
asdfd5465 <forfirestick25@gmail.com>
2025-05-19 12:37:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,5 @@
-// WordFinderApp/build.gradle.kts
-// This file can be intentionally blank if plugins are managed in settings.gradle.kts
-// and applied in module-level build.gradle.kts.
-// However, often you define top-level plugin versions here using the older `buildscript`
-// if not using version catalogs.
-//
-// Given your setup, and if you create libs.versions.toml as hinted above, this works:
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.android.application") version "8.3.0" apply false // Use your verified AGP
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false // Use your verified Kotlin
 }
-
-// If NOT using libs.versions.toml, for this CLI setup, you can do:
-// buildscript {
-//     ext.kotlin_version = "1.9.22" // Example
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-//     dependencies {
-//         classpath 'com.android.tools.build:gradle:8.3.0' // AGP
-//         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-//     }
-// }
-//
-// allprojects {
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
